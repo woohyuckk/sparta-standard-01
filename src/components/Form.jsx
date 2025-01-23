249f8f5fbe03a8fc9848d06a7ae29931615a571a
woohyuckk <woohyuckk@gmail.com>
2025-01-23 12:08:36 +0900

    refactor : 디버그 코딩 제거

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
   
 const Form = ({submitFunc, name, age, setNameHandler,  setAgeHandler}) => {
-    console.log(name)
   return (
     <form onSubmit={submitFunc}>
       {/* TODO: input 태그에 value, onChange 속성을 추가해서 이름과 나이의 상태와 상태변경 로직을 연결하세요 */}
